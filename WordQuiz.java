92c9652f1024491a4b6bea3dbdcc6d5187a59ba8
axebo861 <axebo861@tlhw-4-2.ad.liu.se>
2019-12-06 12:43:55 +0100

    English pseudo code added

--- WordQuiz.java
+++ WordQuiz.java
@@ -7,6 +7,7 @@ public class WordQuiz {
 		dict = dictionary;
 	}
 	public void runMenu() throws IOException, NullPointerException {
+		//Loads document
 		InputStream is = new FileInputStream ("/home/axebo861/eclipse-workspace/lab5/src/ordlista.txt");
 		dict.load(is);
 		Scanner scan = new Scanner(System.in);
@@ -32,29 +33,35 @@ public class WordQuiz {
 				dict.save(os);
 				break;
 			case 3:
+				//Add new words to wordlist
 				Set<Word> words = dict.terms();
 				System.out.println("");
 				System.out.println("Ordlistan är:" + words);
 				break;
 			case 4:
+				//Inverses the wordlist, run and reverts to original
 				dict = dict.inverse();
 				runQuiz();
 				dict = dict.inverse();
 				break;
 			case 5:
+				//Saves and quits the game
 				OutputStream os2 = new FileOutputStream ("/home/axebo861/eclipse-workspace/lab5/src/ordlista.txt");
 				dict.save(os2);
 				System.out.println("Hejdå :(");
 				System.exit(0);
 			default:
+				//If wrong input
 			System.out.println("Fel inmatning. Försök igen.");
 			}
 		}
 	}
 		public void runQuiz() throws IOException, FileNotFoundException {
 		
+			//Counter for correct and incorrect answers
 			int rätt = 0;
 			int fel = 0;
+			//Create two arrays, one with terms and one empty
 			Set<Word> terms = dict.terms();
 			ArrayList<Word> wordList = new ArrayList<Word>(terms);
 			ArrayList<Word> correct = new ArrayList<Word>();
@@ -69,15 +76,17 @@ public class WordQuiz {
 					Set<Word> betydelser = dict.lookup(term);
 					
 					if(betydelser.contains(svarWord)) {
+						//if correct answer, add the answer to empty array
 						System.out.println("Rätt svar! :-)");
-						//lägg rätt svar i correct
 						correct.add(term);
 						rätt++;
 					}else {
+						//wrong answer
 						System.out.println("Fel svar! :-(");
 						fel++;
 					}
 				}			
+				//Removes all correct answers from wordList array
 				for(Word term : correct) {
 					wordList.remove(term);
 				}
@@ -89,7 +98,7 @@ public class WordQuiz {
 			System.out.println("");
 		}
 			public static void main(String[] args) throws IOException {
-				// Skapa en tom ordlista på ngt sätt och fyll den med ord. Dictionary
+				//Create an empty wordlist and fills it with words
 				Dictionary sweng = new Dictionary();
 				WordQuiz wq = new WordQuiz(sweng);
 				wq.runMenu();    
@@ -98,6 +107,7 @@ public class WordQuiz {
 				boolean cont = true;
 				int choice = -1;
 				while(cont) {
+					//meny options
 					System.out.println("1. Spela!");
 					System.out.println("2. Lägg till ord.");
 					System.out.println("3. Skriv ut hela ordlistan.");
